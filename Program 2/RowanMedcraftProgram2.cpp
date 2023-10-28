08c15b63ab7bac17e16244f4f7da87f0e1b4e2e6
Rowan <128566192+rmedcraft@users.noreply.github.com>
2023-10-28 19:11:14 -0400

    Add files via upload

--- Program 2/RowanMedcraftProgram2.cpp
+++ Program 2/RowanMedcraftProgram2.cpp
@@ -16,14 +16,58 @@ struct IndexEntry
    string fName;
    string lName;
    double bal;
+   IndexEntry * left;
+   IndexEntry * right;
 };
 
+void insert(IndexEntry *, int, int, string, string, double);
+IndexEntry* treeSearch(IndexEntry *, int);
+void printNode(IndexEntry *);
+
 void print(priorityQueue, int);
+
 int main()
 {
-	// part 1 here
-	fstream file;
-	file.open("accounts.dat", ios::in);
+	// part 1 starts here
+	IndexEntry * root = new IndexEntry();
+
+	ifstream file;
+	file.open("accounts.dat");
+	if(!file) {
+		cout << "File could not be found" << endl;
+		return 1;
+	}
+	// gets rid of the first line in the .dat file that we dont want to read
+	string useless;
+	getline(file, useless);
+	
+	// reads the data from the file into the BST
+	int index, id;
+	string first, last;
+	double balance;
+	while(file){
+		file >> index >> id >> first >> last >> balance;
+		insert(root, id, index, first, last, balance);
+	}
+
+	// prompts the user to enter an ID to search for
+	cout << "Enter Account ID: ";
+	int input;
+	cin >> input;
+	IndexEntry* foundNode = treeSearch(root, input);
+
+	if(!foundNode){
+		cout << "Matched record number is -1" << endl;
+	} else {
+		cout << "Matched record number is " << foundNode->recNum << endl;
+	}
+	cout << "Corresponding account record from the database file: " << endl;
+	if(!foundNode){
+		cout << "Account ID not in Data" << endl;
+	} else {
+		printNode(foundNode);
+	}
+
 	// part 2 starts here
 	priorityQueue myqueue;
 
@@ -46,6 +90,7 @@ int main()
 	print(myqueue, 91);
 
 	myqueue.min(); // this line of code does absolutely nothing but it was in the driver file so I'm keeping it
+
 	myqueue.extractMin();
 	cout << "After extracting the minimum value, the priority queue has: " << endl;
 	myqueue.printArray();
@@ -78,6 +123,39 @@ int main()
 
 	system("pause");
 	return 0;
+}
+
+void insert(IndexEntry * head, int id, int index, string fName, string lName, double bal){
+    if(!head){
+        head = new IndexEntry();
+		head->acctID = id;
+		head->recNum = index;
+		head->fName = fName;
+		head->lName = lName;
+		head->bal = bal;
+		head->left = NULL;
+		head->right = NULL;
+    } else if(id < head->acctID){
+        insert(head->left, id, index, fName, lName, bal);
+    } else {
+        insert(head->right, id, index, fName, lName, bal);
+    }
+}
+
+IndexEntry* treeSearch(IndexEntry * head, int key){
+	if(!head || key == head->acctID){
+		return head;
+	}
+	if(key < head->acctID){
+		return treeSearch(head->left, key);
+	}
+	return treeSearch(head->right, key);
+}
+
+void printNode(IndexEntry * node){
+	printf("%-12s%-12s%-12s%-12s%-12s\n", "Record#", "AccountID", "FirstName", "LastName", "Balance");
+	printf("%-12d%-12d%-12s%-12s%-12f", node->recNum, node->acctID, node->fName, node->lName, node->bal);
+
 }
 
 void print(priorityQueue queue, int num){
